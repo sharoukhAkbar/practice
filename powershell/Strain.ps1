e838c4fd883cff1ecd8a22d78a8fdfc5b767f7fe
Sharoukh Akbar <sharoukhakbar007@gmail.com>
2026-06-24 15:13:22 +0400

    commit 24062026 1513

---
+++ powershell/Strain.ps1
@@ -0,0 +1,70 @@
+Function Invoke-Keep() {
+    <#
+    .SYNOPSIS
+    Implement the keep operation on collections.
+    
+    .DESCRIPTION
+    Given an array, apply the predicate on each element and return an array of elements where the predicate is true.
+
+    .PARAMETER Data
+    Collection of data to filter through.
+
+    .PARAMETER Predicate
+    The predicate to operate on the data.
+    
+    .EXAMPLE
+    $IsNegative = { param($num) $num -lt 0 }
+    Invoke-Keep -Data @(0, -2, 4, 8, -16) -Predicate $IsNegative
+    Return: @(-2, -16)
+    #>
+    [CmdletBinding()]
+    Param(
+        [Object[]]$Data,
+        [ScriptBlock]$Predicate
+    )
+    
+    $KeepFilteredElements = @()
+
+    foreach($Item in $Data) {
+      if(& $Predicate $Item) {
+        $KeepFilteredElements += ,$Item
+      }
+    }
+    return($KeepFilteredElements)
+}
+
+Function Invoke-Discard() {
+    <#
+    .SYNOPSIS
+    Implement the discard operation on collections.
+    
+    .DESCRIPTION
+    Given an array, apply the predicate on each element and return an array of elements where the predicate is false.
+
+    .PARAMETER Data
+    Collection of data to filter through.
+
+    .PARAMETER Predicate
+    The predicate to operate on the data.
+
+    .EXAMPLE
+    $IsNegative = { param($num) $num -lt 0 }
+    Invoke-Discard -Data @(0, -2, 4, 8, -16) -Predicate $IsNegative
+    Return: @(0, 4, 8)
+    #>
+    [CmdletBinding()]
+    Param(
+        [Object[]]$Data,
+        [ScriptBlock]$Predicate
+    )
+    
+    $DiscardFilteredElements = @()
+
+    foreach($Item in $Data) {
+      if(!(& $Predicate $Item)) {
+        $DiscardFilteredElements += ,$Item
+      }
+    }
+
+    return($DiscardFilteredElements)
+}
