8a0f30a02e054ba888290f4dc290d7c7f4a972e8
Sharoukh Akbar <sharoukhakbar007@gmail.com>
2026-06-26 20:59:03 +0400

    commit 26062026 2058

---
+++ java/hello_world/Greeter.java
@@ -0,0 +1,12 @@
+/** Return Hello, World! */
+
+class Greeter {
+
+    String getGreeting() {
+        return "Hello, World!";
+    }
+    public static void main(String[] args) {
+      Greeter greeter = new Greeter();
+      System.out.println(greeter.getGreeting());      
+    }
+}
